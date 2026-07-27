0964636b9fcea79b290832899a15f5a74ad0f2f0
Victor Kioko <victorsila227@gmail.com>
2026-07-27 11:14:50 +0300

    Add temporary debug logging

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
-require('dotenv').config();
+console.log('DATABASE_URL present:', !!process.env.DATABASE_URL);
+console.log('DATABASE_URL starts with:', process.env.DATABASE_URL?.slice(0, 15));
 const express = require('express');
 const cors = require('cors');
 const sequelize = require('./config/db');
